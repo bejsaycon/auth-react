4e521e00cd3dc89365e97f048d70b164b9382a26
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-08 20:04:41 +0800

    Context and sign in sign out

--- src/RandomUserApp/RandomApp.js
+++ src/RandomUserApp/RandomApp.js
@@ -1,14 +1,23 @@
 import {useState, useCallback} from "react";
+import './styles/RandomApp.css'
 import Welcome from "./components/Welcome";
 import Outputbox from "./components/Outputbox";
 import InputForm from "./components/InputForm";
 import { useFetch } from "./custom_hooks/useFetch";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./custom_hooks/useAuth";
 
 function RandomApp() {
   const [seed, setSeed] = useState('');
   const url = `https://randomuser.me/api/?seed=${seed}`;
   const data = useFetch(url);
+  const navigate = useNavigate();
+  const {setAuth} = useAuth();
   
+  const logout = async () => {
+    setAuth(false);
+    navigate('/login');
+}
   const handleSubmit = useCallback(
     (event) => {
       event.preventDefault();
@@ -22,6 +31,8 @@ function RandomApp() {
     <div className="container">
       <InputForm handleSubmit={handleSubmit}/>
       {seed==='' ? <Welcome /> :  <Outputbox usersData={data} />}
+                <button onClick={logout}>Sign Out</button>
+            
     </div>
   );
 }
